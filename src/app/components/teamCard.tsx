e888a616cab2be326928ace314b87ca38972772c
Mike Spensieri <mspensieri@zendesk.com>
2026-08-28 09:25:32 -0400

    Extract reusable team card

---
+++ src/app/components/teamCard.tsx
@@ -0,0 +1,201 @@
+import { useContext } from "react";
+
+import PlayerContext from "../context/playerContext";
+import RuleSetContext from "../context/ruleSetContext";
+import { TeamScore } from "../data/types";
+import { captainNames, stars } from "../utils/format";
+import { SWAP_DEADLINE } from "../data/teams";
+import PlacementChart from "./placementChart";
+
+const styles: Record<string, React.CSSProperties> = {
+  indicatorGreen: {
+    color: "var(--green-indicator-color)",
+  },
+  indicatorRed: {
+    color: "var(--red-indicator-color)",
+  },
+  rank: {
+    fontSize: "12pt",
+    padding: "2px 6px",
+    borderRadius: "var(--border-radius-standard)",
+    backgroundColor: "var(--component-background-color-secondary)",
+  },
+  placementHistory: {
+    paddingTop: "8px",
+    gridColumn: "span 2",
+    justifySelf: "end",
+    marginRight: "-4px",
+  },
+  rankContainer: {
+    justifySelf: "center",
+  },
+  teamName: {
+    textAlign: "center",
+    justifySelf: "center",
+    gridRow: "span 2",
+    gridColumn: "span 2",
+  },
+  captain: {
+    fontSize: "9pt",
+    color: "var(--component-text-color-secondary)",
+  },
+  points: {
+    textAlign: "center",
+    justifySelf: "center",
+  },
+  playerCount: {
+    fontSize: "11pt",
+    gridColumn: "span 2",
+    justifySelf: "start",
+    alignSelf: "end",
+    paddingTop: "8px",
+  },
+  diff: {
+    justifySelf: "center",
+    fontSize: "8pt",
+  },
+  units: {
+    fontSize: "10pt",
+  },
+  divider: {
+    borderBottom: "1px solid var(--component-text-color-secondary)",
+    gridColumn: "span 4",
+  },
+  inactivePlayer: {
+    opacity: 0.2,
+  },
+};
+
+function UserIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="size-4"
+      style={{ display: "inline" }}
+    >
+      <path
+        fillRule="evenodd"
+        d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export default function TeamCard(props: {
+  thisWeekScore: TeamScore;
+  lastWeekScore: TeamScore | undefined;
+  currentWeek: number;
+  onClick?: (teamScore: TeamScore) => void;
+  active?: boolean;
+}) {
+  const { thisWeekScore, lastWeekScore, currentWeek, onClick, active } = props;
+  const ruleSet = useContext(RuleSetContext);
+  const playerRankings = useContext(PlayerContext);
+
+  function getScoreDiff() {
+    const scoreDiff =
+      thisWeekScore[ruleSet].total -
+      (lastWeekScore ? lastWeekScore[ruleSet].total : 0);
+
+    if (scoreDiff > 0) {
+      return (
+        <>
+          <span style={styles.indicatorGreen}>
+            (+
+            {scoreDiff})
+          </span>
+        </>
+      );
+    } else {
+      return "(-)";
+    }
+  }
+
+  function getRankDiff() {
+    if (lastWeekScore) {
+      if (thisWeekScore[ruleSet].rank < lastWeekScore[ruleSet].rank) {
+        return (
+          <>
+            <span style={styles.indicatorGreen}>
+              (▲ {lastWeekScore[ruleSet].rank - thisWeekScore[ruleSet].rank})
+            </span>
+          </>
+        );
+      } else if (thisWeekScore[ruleSet].rank > lastWeekScore[ruleSet].rank) {
+        return (
+          <>
+            <span style={styles.indicatorRed}>
+              (▼ {thisWeekScore[ruleSet].rank - lastWeekScore[ruleSet].rank})
+            </span>
+          </>
+        );
+      } else {
+        return "(-)";
+      }
+    } else {
+      return "(-)";
+    }
+  }
+
+  const { team } = thisWeekScore;
+  const swapAvailable =
+    (!team.swap && currentWeek < SWAP_DEADLINE) ||
+    (team.swap && team.swap.week > currentWeek);
+
+  const players = [...team.players].concat(
+    team.swap && team.swap.week <= currentWeek ? [team.swap.playerIn] : [],
+  );
+  const activePlayers = players.filter((player) => {
+    const playerScore = playerRankings[currentWeek].find(
+      (r) => r.player === player,
+    );
+
+    return playerScore && !["eliminated", "jury"].includes(playerScore.status);
+  });
+
+  return (
+    <div
+      className={`flex-item-card team-grid-container ${active ? "active" : ""}`}
+      onClick={() => onClick?.(thisWeekScore)}
+    >
+      <div style={styles.rankContainer}>
+        <span style={styles.rank}>{`#${thisWeekScore[ruleSet].rank}`}</span>
+      </div>
+      <div style={styles.teamName}>
+        {team.name} <br />
+        <span style={styles.captain}>
+          {captainNames(team)} ({stars(team.history.overallRating)})
+        </span>
+      </div>
+      <div style={styles.points}>
+        {thisWeekScore[ruleSet].total}
+        <span style={styles.units}>pts</span>
+      </div>
+      <div style={styles.diff}>{getRankDiff()}</div>
+      <div style={styles.diff}>{getScoreDiff()}</div>
+      <div style={styles.divider}></div>
+      <div style={styles.playerCount}>
+        <UserIcon />x{activePlayers.length}
+        {swapAvailable && <> (+swap)</>}
+        {/* {activePlayers.map((_, i) => (
+                  <UserIcon key={i} />
+                ))}
+                {swapAvailable && (
+                  <span style={styles.inactivePlayer}>
+                    <UserIcon />
+                  </span>
+                )} */}
+      </div>
+      <div style={styles.placementHistory}>
+        <PlacementChart
+          team={team}
+          currentWeek={currentWeek}
+          simple={true}
+        ></PlacementChart>
+      </div>
+    </div>
+  );
+}
